78e5e0995154f3b487550fc2e6fc2449124f5feb
Andrew Souza <andrew.souza102024@gmail.com>
2025-10-08 20:01:28 -0300

    Finalizando a responsividade do projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Mostra spans gerados automaticamente atraves de um temporizador
 const roles = ["JavaScript", "TypeScript", "Node.js"];
 let i = 0;
 const span = document.querySelector(".typing");
@@ -6,3 +7,12 @@ setInterval(() => {
   span.textContent = roles[i];
   i = (i + 1) % roles.length;
 }, 2000);
+
+
+// abre/fecha o menu ao clicar no logo
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
